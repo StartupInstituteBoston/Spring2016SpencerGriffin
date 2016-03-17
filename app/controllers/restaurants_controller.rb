796343d411e15ea28171260fecfaa61c2e44b680
Spencer Griffin <spencekarl@gmail.com>
2016-03-17 07:08:22 -0400

    updated create method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,6 +14,12 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render 'new'
+    end
   end
 
   def edit
