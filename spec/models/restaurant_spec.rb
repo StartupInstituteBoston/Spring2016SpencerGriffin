5d3346248ac061e35946a5f1c0130ffd8eaf135a
Spencer Griffin <spencekarl@gmail.com>
2016-03-18 16:34:19 -0400

    updated rspec test

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let :restaurant do
+    Restaurant.new(name: 'test',
+                   description: 'test',
+                   phone: '123-123-1234',
+                   address: 'test',
+                   owner_id: 1)
+  end
+
+  it 'should create new restaurant' do
+    expect( restaurant.save! ).to be_truthy
+  end
+
+  it 'should validate presense of name' do
+    restaurant.name = nil
+    expect( restaurant ).to be_invalid
+  end
+
+  it 'should validate presense of name' do
+    restaurant.phone = nil
+    expect( restaurant ).to be_invalid
+  end
 end
