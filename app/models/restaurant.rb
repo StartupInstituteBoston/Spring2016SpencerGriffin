78466b4cef14f7663b78d554edbe38afe350a352
Spencer Griffin <spencekarl@gmail.com>
2016-03-18 15:40:20 -0400

    added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
-  validates :name, presence: true
-  validates :phone, presence: true, length: { minimum: 10 }
+  validates_presence_of :name, :description, :owner_id, :phone
+  validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "invalid"}
 
   belongs_to :owner
 end
